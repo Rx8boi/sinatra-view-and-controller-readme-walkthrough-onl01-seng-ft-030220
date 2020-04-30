7d59e744331e2a9525aa418b9981d8f87f696a7c
Charles Butler <cdbutle@outlook.com>
2020-04-30 05:08:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,11 +6,12 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    puts params[:string]
     erb :reversed
   end
 
   get '/friends' do
-    erb :friends# Write your code here!
-
+    erb :friends
+    # Write your code here!
   end
 end
